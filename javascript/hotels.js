857299e39309be6eea6fd1aaf914d6960c4d9c8e
miraphilip5 <miirapanji5@gmail.com>
2022-04-19 19:47:12 -0400

    code to deploy

---
+++ javascript/hotels.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "<img src= '../images/foods.jpg' width=\'300px\' height=\'200px\'>";
+  }
+  function myFunction1() {
+    document.getElementById("demo1").innerHTML = "<img src= '../images/refreshments.jpg' width=\'300px\' height=\'200px\'>";
+  }
+  function myFunction2() {
+    document.getElementById("demo2").innerHTML = "<img src= '../images/security.jpg' width=\'400px\' height=\'200px\'>";
+  }
+  
+  function Redirect1() {
+    window.location.assign("http://127.0.0.1:5500/pages/bookHotels.html");
+  }
